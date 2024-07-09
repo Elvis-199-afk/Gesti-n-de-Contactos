a486e5247e5ff71153d1a87a42958a9ad9db9e23
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-09 09:31:19 -0500

    Agregando cuarta funcionalidad, que trata de ordenar los contactos por servidor de correo

--- Gestion_de_contactos.cpp
+++ Gestion_de_contactos.cpp
@@ -58,6 +58,98 @@ void mostrarLista(contactoEmail *contacto, int n){
 		cout<<contacto[i].nacionalidad<<"\t\t\n";
 	}
 }
+int tamArreglo(contactoEmail contacto[], int x){
+	int n=-1;
+	for(int i=0; contacto[x].email[i]!='\0'; i++){
+		n++;
+	}
+	return n;
+}
+void mostrarListaOrdenada(contactoEmail arr[], int n){
+	int gmail[10], outlook[10], yahoo[10], etc[10], otros=-1, ng=-1, nk=-1, ny=-1;
+	for(int i=0;i<=n;i++){
+		int tam;
+		tam=tamArreglo(arr, i);
+		if(arr[i].email[tam-4]=='l'){	
+			ng++;
+			gmail[ng]=i;
+		}else if(arr[i].email[tam-4]=='k'){
+			nk++;
+			outlook[nk]=i;
+		}else if(arr[i].email[tam-4]=='o'){
+			ny++;
+			yahoo[ny]=i;
+		}else {
+			otros++;
+			etc[otros]=i;
+		}
+	}
+	
+	cout<<"Gmail: "<<endl;
+	if(ng>-1){
+		cout<<"NOMBRE\t\t"<<"SEXO\t\t"<<"EDAD\t\t"<<"TELEFONO\t\t"<<"EMAIL\t\t"<<"NACIONALIDAD\t\t\n\n";
+		for(int i=0;i<=ng;i++){
+			cout<<arr[gmail[i]].nombreCompleto<<"\t\t";
+			cout<<arr[gmail[i]].sexo<<"\t\t";
+			cout<<arr[gmail[i]].edad<<"\t\t";
+			cout<<arr[gmail[i]].telefono<<"\t\t";
+			cout<<arr[gmail[i]].email<<"\t\t";
+			cout<<arr[gmail[i]].nacionalidad<<"\t\t\n";
+		}
+		cout<<endl;
+	}else{
+		cout<<"No existen contactos...\n\n";
+	}
+	
+	cout<<"Outlook: "<<endl;
+	if(nk>-1){
+		cout<<"NOMBRE\t\t"<<"SEXO\t\t"<<"EDAD\t\t"<<"TELEFONO\t\t"<<"EMAIL\t\t"<<"NACIONALIDAD\t\t\n\n";
+		for(int i=0;i<=nk;i++){
+			cout<<arr[outlook[i]].nombreCompleto<<"\t\t";
+			cout<<arr[outlook[i]].sexo<<"\t\t";
+			cout<<arr[outlook[i]].edad<<"\t\t";
+			cout<<arr[outlook[i]].telefono<<"\t\t";
+			cout<<arr[outlook[i]].email<<"\t\t";
+			cout<<arr[outlook[i]].nacionalidad<<"\t\t\n";
+		}
+		cout<<endl;
+	}else{
+		cout<<"No existen contactos...\n\n";
+	}
+
+	cout<<"Yahoo: "<<endl;
+	if(ny>-1){
+		cout<<"NOMBRE\t\t"<<"SEXO\t\t"<<"EDAD\t\t"<<"TELEFONO\t\t"<<"EMAIL\t\t"<<"NACIONALIDAD\t\t\n\n";
+		for(int i=0;i<=ny;i++){
+			cout<<arr[yahoo[i]].nombreCompleto<<"\t\t";
+			cout<<arr[yahoo[i]].sexo<<"\t\t";
+			cout<<arr[yahoo[i]].edad<<"\t\t";
+			cout<<arr[yahoo[i]].telefono<<"\t\t";
+			cout<<arr[yahoo[i]].email<<"\t\t";
+			cout<<arr[yahoo[i]].nacionalidad<<"\t\t\n";
+		}
+		cout<<endl;
+	}else{
+		cout<<"No existen contactos...\n\n";
+	}
+	
+	cout<<"Otros: "<<endl;
+	if(otros>-1){
+		cout<<"NOMBRE\t\t"<<"SEXO\t\t"<<"EDAD\t\t"<<"TELEFONO\t\t"<<"EMAIL\t\t"<<"NACIONALIDAD\t\t\n\n";
+		for(int i=0;i<=otros;i++){
+			cout<<arr[etc[i]].nombreCompleto<<"\t\t";
+			cout<<arr[etc[i]].sexo<<"\t\t";
+			cout<<arr[etc[i]].edad<<"\t\t";
+			cout<<arr[etc[i]].telefono<<"\t\t";
+			cout<<arr[etc[i]].email<<"\t\t";
+			cout<<arr[etc[i]].nacionalidad<<"\t\t\n";
+		}
+		cout<<endl;
+	}else{
+		cout<<"No existen contactos...\n\n";
+	}
+}
+
 int main(){
 	contactoEmail conE[30];
 	int n=-1;
@@ -90,8 +182,21 @@ int main(){
 				system("pause");
 				break;
 			case 3:
-				mostrarLista(conE,n);
-				cout<<endl;
+				if(n==-1){
+					cout<<"No hay contactos existen...\n ";	
+				}else{
+					mostrarLista(conE,n);
+					cout<<endl;
+				}
+				system("pause");
+				break;
+			case 4:
+				if(n==-1){
+					cout<<"No hay contactos existen...\n ";
+				}else{
+					cout<<"El listado de contactos existentes, ordenado por servidor de correo de las cuentas (gmail.com, outlook.com, yahoo.com, etc.) es: \n\n";
+					mostrarListaOrdenada(conE,n);
+				}
 				system("pause");
 				break;
 			case 5:
